c2e5095d8819474048e29fd0fbff17d009f99b3c
Chandravijay  rai <82499697+Cvr421@users.noreply.github.com>
2022-11-15 12:18:58 +0530

    Create index.js

---
+++ Client/Eth_pay/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { TransactionsProvider } from "./context/TransactionContex";
+import "./index.css";
+// import {TransactionsProvider} from './context/TransactionContex';
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <TransactionsProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TransactionsProvider>
+);
